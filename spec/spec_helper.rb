a9c41b8217be31ab343f328713a142ad38a61a97
Michael Matheis <githubot@matheis.work>
2026-05-20 00:28:20 +0200

    Fix Ruby 3.2 test harness issues for surveyor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -81,7 +81,12 @@ RSpec.configure do |config|
   # end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
+  fixtures_path = "#{::Rails.root}/spec/fixtures"
+  if config.respond_to?(:fixture_paths=)
+    config.fixture_paths = [fixtures_path]
+  else
+    config.fixture_path = fixtures_path
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
